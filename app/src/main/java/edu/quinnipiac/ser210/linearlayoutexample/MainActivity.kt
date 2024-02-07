54f0a576fa84eadec0af57e0e5fe73d85504c471
charlizeaponte <charlizeaponte@yahoo.com>
2024-02-06 20:02:27 -0500

    Linear Layout: Chapter 3 demo

---
+++ app/src/main/java/edu/quinnipiac/ser210/linearlayoutexample/MainActivity.kt
@@ -0,0 +1,18 @@
+/**
+ * @author Charlize Aponte
+ * 2/6/24
+ * Assignment: Chapter 3 Demo: Linear Layout Example
+ * The program demonstrates the use of Linear Layout
+ */
+
+package edu.quinnipiac.ser210.linearlayoutexample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
